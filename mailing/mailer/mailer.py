9e221de208a7d4f8165651f21347a53f827e5acd
AdamJC20 <adamcummings20@googlemail.com>
2025-11-18 09:24:50 +0000

    changed structure in line with new architecture diagram

--- mailing/mailer/mailer.py
+++
@@ -1,9 +0,0 @@
-"""Lambda function for creating and sending emails about discounts using SES"""
-
-
-def lambda_handler(event, context):
-    return {'event': event, 'context': context}
-
-
-if __name__ == "__main__":
-    pass
