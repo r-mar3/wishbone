739a1152554ea5c845df1307c7752e1d4d201edb
DevMjee <dev.mjee2@gmail.com>
2025-11-18 18:10:45 +0000

    removing steam user api key necessity

--- source/pipeline/extract_steam.py
+++ source/pipeline/extract_steam.py
@@ -7,7 +7,10 @@ import json
 import numpy as np
 import pandas as pd
 
-URL_FORMAT = "https://store.steampowered.com/api/appdetails/?appids={appid}&cc=uk"
+URL_FORMAT = "https://store.steampowered.com/api/appdetails/?appids={app_ids}&cc=uk"
+# for boolean checking
+PRICE_FORMAT = "https://store.steampowered.com/api/appdetails/?appids={app_id}&cc=uk"
+
 # WISHLIST_URL_FORMAT = "https://api.steampowered.com/IWishlistService/GetWishlist/v1/?access_token={api_key}&steamid={steamid}"
 MAX_RESULTS = 10000
 NUM_PROCESSES_INFO = 16
@@ -19,15 +22,15 @@ def get_from_steam_db(app_ids: list[int]) -> dict:
     app_ids = [str(app_id) for app_id in app_ids]
     app_ids_csv = ','.join(app_ids)
     # print(app_ids_csv)
-    response = requests.get(PRICE_URL_FORMAT.format(app_ids=app_ids_csv))
+    response = requests.get(URL_FORMAT.format(app_ids=app_ids_csv))
     # response_list = [{k: v} for k, v in response.json().items()]
     return response.text
 
 
-def is_valid_steam_endpoint(appid: int) -> bool:
+def is_valid_steam_endpoint(app_id: int) -> bool:
     """Pull games, ids, and prices from database"""
-    response = requests.get(URL_FORMAT.format(appid=appid))
-    return response.json().get(str(appid)).get('success')
+    response = requests.get(PRICE_FORMAT.format(app_id=app_id))
+    return response.json().get(str(app_id)).get('success')
 
 # def get_wishlist_by_id(steamid: int) -> list[dict]:
 #     response = requests.get(WISHLIST_URL_FORMAT.format(
@@ -35,56 +38,22 @@ def is_valid_steam_endpoint(appid: int) -> bool:
 #     print(response.json())
 
 
-def check_new_endpoints() -> int:
+def check_new_endpoints() -> list[int]:
+    pages = []
     app_id = 10
     failures = 100
     while failures > 0:
-        try:
-            if is_valid_steam_endpoint(app_id):
-                failures -= 1
-            app_id += 10
-        except:
-            continue
-
-
-def check_new_endpoints() -> list[int]:
-    is_more_results = True
-    page_index = 0
-    pages = [0]
-    while is_more_results:
-        response = requests.get(GAME_INFO_URL_FORMAT.format(
-            api_key=environ['API_KEY'], max_results=MAX_RESULTS, page_index=page_index))
-        output = response.json()
-        is_more_results = output.get('response').get('have_more_results')
-
-        if is_more_results:
-            last_appid = output.get('response').get('last_appid')
-            page_index = last_appid
-            pages.append(last_appid)
+        if is_valid_steam_endpoint(app_id):
+            pages.append(app_id)
+        else:
+            failures -= 1
+        app_id += 10
 
     print("no more results")
     print(f"last app id = {app_id}")
     print(f"pages = {pages}")
 
-    return app_id
     return pages
-
-
-def fetch_game_info_page(page_index: int) -> list[dict]:
-    response = requests.get(
-        GAME_INFO_URL_FORMAT.format(api_key=environ['API_KEY'], max_results=MAX_RESULTS, page_index=page_index))
-
-    return response.json().get('response').get('apps')
-
-
-def get_game_info() -> list[dict]:
-    pages = check_new_endpoints()
-    with multiprocessing.Pool(NUM_PROCESSES_INFO) as pool:
-        output = pool.map(fetch_game_info_page, pages)
-
-    # output = fetch_game_info_page(0)
-    output = list(np.concatenate(output))
-    return output
 
 
 def get_price_info(app_ids_matrix: list[list[int]]) -> list[dict]:
@@ -96,14 +65,8 @@ def get_price_info(app_ids_matrix: list[list[int]]) -> list[dict]:
 
 if __name__ == "__main__":
     start_time = time.time()
-    load_dotenv()
-    output = get_game_info()
-    df = pd.DataFrame(output)
-    with open("data/a_file.json", 'w', encoding='utf-8') as f:
-        json.dump(output, f, indent=4)
 
-    print(df)
-    app_ids = list(df['appid'])
+    app_ids = check_new_endpoints()
     app_ids_matrix = np.array_split(app_ids, 2500)
     app_ids_matrix = [app_ids[x:x+25] for x in range(0, len(app_ids), 25)]
 
