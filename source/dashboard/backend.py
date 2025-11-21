1105de52e58a124daa54a45d7ea86ee39eed69b2
AdamJC20 <adamcummings20@googlemail.com>
2025-11-21 12:27:03 +0000

    moved backend functions to separate file 'backend.py

---
+++ source/dashboard/backend.py
@@ -0,0 +1,32 @@
+import aioboto3
+import json
+import asyncio
+
+
+async def trigger_lambda(payload: dict) -> dict:
+    async with aioboto3.client('lambda') as client:
+        response = await client.invoke(
+            FunctionName='wishbone-tracking-lambda',
+            InvocationType='RequestResponse',
+            Payload=json.dumps(payload)
+        )
+
+    response_payload = await response['Payload'].read()
+    return json.loads(response_payload)
+
+
+def run_unsubscribe(email: str):
+    payload = {
+        'subscribe': 'False',
+        'email': email
+    }
+    return asyncio.run(trigger_lambda(payload))
+
+
+def run_subscribe(email: str, game_id):
+    payload = {
+        'subscribe': 'False',
+        'email': email,
+        'game_id': str(game_id)
+    }
+    return asyncio.run(trigger_lambda(payload))
